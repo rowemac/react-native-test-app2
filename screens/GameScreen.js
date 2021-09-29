aedb67efe71d0c9913f01996346e801151dcd63f
Mac Rowe <m.rowe42@gmail.com>
2021-09-29 14:29:05 -0400

    Add buttons to GameScreen component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min); 
@@ -19,13 +20,30 @@ const GameScreen = props => {
     );
 
     return (
-        <View>
+        <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="LOWER" onPress={() => {}} />
+                <Button title="GREATER" onPress={() => {}} /> 
+            </Card>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1, 
+        padding: 10, 
+        alignItems: 'center'
+    }, 
+    buttonContainer: {
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%' 
+    }
+});
 
 export default GameScreen; 
