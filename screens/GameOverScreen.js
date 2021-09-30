47bb3993d689af575f8736fa48ffb26e87457230
Mac Rowe <m.rowe42@gmail.com>
2021-09-30 15:06:30 -0400

    Add image to GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from 'react-native'; 
+import { View, Text, StyleSheet, Button, Image } from 'react-native'; 
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>Game Over</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
+            <TitleText>Game Over</TitleText>
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={require('../assets/success.png')}
+                    // source={{
+                    //     uri: 
+                    //     'https://images.fineartamerica.com/images-medium-large-5/climber-standing-on-a-mountain-summit-buena-vista-images.jpg'
+                    // }}
+                    // style={styles.image} -- for network images --
+                    resizeMode="cover"
+                />
+            </View>
+            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+            <BodyText>Number was: {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     );
@@ -17,6 +30,21 @@ const styles = StyleSheet.create({
         flex: 1, 
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150, 
+        borderWidth: 3, 
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 
