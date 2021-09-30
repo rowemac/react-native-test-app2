9beb9fb5c5203eb1d41252f1e167fa6c78b96ea6
Mac Rowe <m.rowe42@gmail.com>
2021-09-30 15:42:34 -0400

    Create MainButton custom button component and implement

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, Button, Image } from 'react-native'; 
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import MainButton from "../components/MainButton";
 import Colors from "../constants/Colors";
 
 const GameOverScreen = props => {
@@ -25,7 +26,7 @@ const GameOverScreen = props => {
                     number <Text style={styles.highlight}>{props.userNumber}</Text>
                 </BodyText>
             </View>
-            <Button title="NEW GAME" onPress={props.onRestart} />
+            <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
         </View>
     );
 };
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
+import MainButton from "../components/MainButton";
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min); 
@@ -56,8 +57,12 @@ const GameScreen = props => {
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={() => nextGuessHandler('lower')} />
-                <Button title="GREATER" onPress={() => nextGuessHandler('greater')} /> 
+                <MainButton onPress={() => nextGuessHandler('lower')}>
+                    LOWER
+                </MainButton>
+                <MainButton onPress={() => nextGuessHandler('greater')}>
+                    GREATER
+                </MainButton>
             </Card>
         </View>
     );
@@ -73,8 +78,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row', 
         justifyContent: 'space-around', 
         marginTop: 20,
-        width: 300,
-        maxWidth: '80%' 
+        width: 400,
+        maxWidth: '90%' 
     }
 });
 
