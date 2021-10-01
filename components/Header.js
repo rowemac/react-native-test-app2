88a55afb65519a78bdb8bb5aa7427b102cf8e2c5
Mac Rowe <m.rowe42@gmail.com>
2021-10-01 15:08:54 -0400

    Make minor style changes via ternary operators in styles on Header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import TitleText from './TitleText';
 import Colors from '../constants/Colors';
 
 const Header = props => {
     return (
         <View style={styles.header}>
-            <TitleText style={styles.headerTitle}>{props.title}</TitleText>
+            <TitleText style={styles.title}>{props.title}</TitleText>
         </View>
     );
 };
@@ -16,10 +16,16 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90, 
         paddingTop: 36,
-        backgroundColor: Colors.primary,
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
         alignItems: 'center', 
-        justifyContent: 'center'
+        justifyContent: 'center', 
+        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent', 
+        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
     }, 
+
+    title: {
+        color: Platform.OS === 'ios' ? Colors.primary : 'white'
+    }
 });
 
 export default Header;
